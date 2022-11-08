d5fbfecb3209ca9f524fec9fd1e57db34a164b1d
jannat456nur <jannatjenny22@gmail.com>
2022-11-08 20:29:14 +0600

    test

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { colors } from "./src/theme/colors";
+import { spacing } from "./src/theme/spacing";
+import { useFonts } from "expo-font";
+import { typography } from "./src/theme/typography";
+import Text from "./src/components/text/text";
+import { NavigationContainer,DarkTheme } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./src/screens/Home";
+import Details from "./src/screens/Details";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [loaded] = useFonts({
+    "Antonio-medium": require("./assets/Font/Antonio-medium.ttf"),
+    // 'LeagueSpartan-Bold.ttf':require('./assets/Font/LeagueSpartan-Bold.ttf'),
+    "LeagueSpartan-regular.ttf": require("./assets/Font/LeagueSpartan-regular.ttf"),
+  });
+
+  if (!loaded) {
+    return <Text>Font is Loading....</Text>;
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+{/*    
+    <NavigationContainer theme={DarkTheme}>
+      <Stack.Navigator screenOptions={{headerShown:false}}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+    <StatusBar style="light"/> */}
+
+    <Text preset="h1" style={{backgroundColor:"black",marginTop:100,color:'white'}}>
+      Please Do It.
+    </Text>
+    <Text preset="">
+      welcome
+    </Text>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: colors.lightGreen,
+    //ackgroundColor:"black",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
